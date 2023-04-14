8338bde59be50b0506da2394d3a45f99accbc1c4
Sasha Pylypenko <Sashapylypenko.pov@gmail.com>
2023-04-14 15:36:16 +0300

    fix: slick slider

---
+++ src/js/01_main.js
@@ -0,0 +1,24 @@
+console.log('js work!');
+
+$(document).ready(function () {
+    $(".team-slider").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        responsive: [
+            {
+              breakpoint: 1440,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+          ]
+      });
+  });
