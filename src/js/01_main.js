625e570042605abd94dfb559eb4a64d026944e0f
Sasha Pylypenko <Sashapylypenko.pov@gmail.com>
2023-04-15 13:31:58 +0300

    fix: main screen

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,6 +1,16 @@
 console.log('js work!');
 
 $(document).ready(function () {
+  if(window.screen.width >= 1440){
+    $(".header__input").focus(() =>{
+      console.log('focused')
+   $(".header .nav").hide();
+    })
+    $(".header__input").focusout(() =>{
+      console.log('unfocused')
+      $(".header .nav").show();
+    })
+  }
     $(".team-slider").slick({
         dots: true,
         infinite: true,
